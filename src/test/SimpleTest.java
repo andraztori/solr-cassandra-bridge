e3bfe8c75e2a5aedcc24c95c3ddf94e4943deff4
Andraz Tori <andraz@zemanta.com>
2014-02-16 14:14:53 +0100

    Implement basic testing infrastrucutre

---
+++ src/test/SimpleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SimpleTest
+{
+	@Test
+	public void check()
+	{
+		int a = 1;
+		assert(true);
+		assertEquals(true, true);
+	}
+}
